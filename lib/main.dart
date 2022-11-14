80507f617f7b93bf45786b3cf8b74ecf1bba1430
hiroshi <gonzalesromanhiroshiyaisen@gmail.com>
2022-11-14 13:34:34 -0500

    primer commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Base de Datos App',
+      title: 'BaseDatos App',
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
